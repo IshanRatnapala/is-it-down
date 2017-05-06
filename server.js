841e538c0dbe64f01bc89f2d24a228c865519e99
Dragfreeze <Dragfreeze@gmail.com>
2017-05-06 12:48:30 +0530

    serving favicons

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 const PORT = 3002;
 const app = express();
 
 app.set('view-engine', 'ejs');
-app.use(express.static(__dirname + '/public', { maxage: '5d' }));
-
+app.use(favicon(path.join(__dirname, 'public/favicon', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public'), { maxage: '5d' }));
 
 require('./routes/main-routes')(app);
 require('./routes/error')(app);
