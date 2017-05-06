4aab8d5939b0fd06922a15dca33181c53b185149
Dragfreeze <Dragfreeze@gmail.com>
2017-05-06 12:38:02 +0530

    do request using http

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,4 +1,5 @@
-const http = require('http');
+// const http = require('http');
+const request = require('request');
 
 function parseStatusCode (statusCode) {
     const code = statusCode.toString();
@@ -22,7 +23,7 @@ function parseStatusCode (statusCode) {
     }
     if (code[0] === '5') {
         return {
-            message: 'The server is live',
+            message: 'The server is live but there is a Internal Server Error',
             additional: 'Server responded with a status code of ' +  code + ' which means there are server errors.'
         };
     }
@@ -32,6 +33,12 @@ function parseStatusCode (statusCode) {
         return {
             message: 'Domain name not found',
             additional: ''
+        };
+    }
+    if (code === 'ETIMEDOUT') {
+        return {
+            message: 'Connection Timeout',
+            additional: 'No response from the server for 15 seconds.'
         };
     }
 
@@ -49,25 +56,88 @@ module.exports = (app) => {
         });
     });
     app.get('/:site', (req, res) => {
-        var options = {
-            method: 'HEAD',
-            host: req.params.site,
-            port: 80,
-            path: '/'
-        };
+        console.log(req.params.site);
+        doRequest(req.params.site, res);
+    });
+};
 
-        http.request(options, function (response) {
+function configOptions (site) {
+    let URI = site;
+
+    let options = {
+        method: 'HEAD',
+        port: 80,
+        path: '/'
+    };
+
+    if (URI.indexOf('://') >= 0) {
+        options.protocol = URI.split('://')[0] + ':';
+        URI = URI.split('://')[1];
+    }
+
+    let host = URI.split('/')[0];
+    options.host = host;
+    URI = host;
+
+    let port = URI.split(':')[1];
+    if (port) {
+        options.port = port;
+    }
+
+    return options;
+}
+
+function doRequest (site, res) {
+    const options = configOptions(site);
+    // const options = configOptions('https://www.monoprice.com/asdasd')''
+
+    http.request(options, function (response) {
+        console.log(response.headers);
+
+        if (response.statusCode.toString()[0] === '3') {
+            console.log(response.headers.location);
+            doRequest(response.headers.location, res);
+        } else {
             res.render('pages/detail-page.ejs', {
                 pageTitle: 'Is it down?',
                 content: parseStatusCode(response.statusCode, response.headers)
             });
+        }
+    })
+        .on('error', function (err) {
+            res.render('pages/detail-page.ejs', {
+                pageTitle: 'Is it down?',
+                content: parseStatusCode(err.code, err)
+            });
         })
-            .on('error', function (err) {
-                res.render('pages/detail-page.ejs', {
-                    pageTitle: 'Is it down?',
-                    content: parseStatusCode(err.code, err)
-                });
-            })
-            .end();
-    });
-};
+        .end();
+}
+//
+// function doRequest (site, res) {
+//     if (site.indexOf('://') < 0) {
+//         site = 'http://' + site;
+//     }
+//
+//     request(site, {timeout: 15000}, function (error, response, body) {
+//         console.log('error:', error); // Print the error if one occurred
+//         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//         // console.log('body:', body); // Print the HTML for the Google homepage.
+//
+//         if (error) {
+//             res.render('pages/detail-page.ejs', {
+//                 pageTitle: 'Is it down?',
+//                 content: parseStatusCode(error.code, error)
+//             });
+//         }
+//
+//         if (response.statusCode.toString()[0] === '3') {
+//             console.log(response.headers.location);
+//             doRequest(response.headers.location, res);
+//         } else {
+//             res.render('pages/detail-page.ejs', {
+//                 pageTitle: 'Is it down?',
+//                 content: parseStatusCode(response.statusCode, response.headers)
+//             });
+//         }
+//     });
+// }
